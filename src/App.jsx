0e373a628141623570a16d17968bd0be26ecfa3a
vedant224 <vedanthande2244@gmail.com>
2025-09-03 21:05:11 +0530

    adding project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <video 
+      src="https://ik.imagekit.io/vedant224/videoplayer/test.mp4?tr=w-800"
+      width="800"
+      height="450"
+      controls
+      autoPlay
+      muted
+      loop
+      />
+    </div>
+  )
+}
+
+export default App
